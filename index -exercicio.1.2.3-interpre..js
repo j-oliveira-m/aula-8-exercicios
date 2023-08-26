140e71029f45c17e9bc176c399bf79d0988a5cb4
j-oliveira-m <141836902+j-oliveira-m@users.noreply.github.com>
2023-08-26 19:56:02 -0300

    Add files via upload

---
+++ index -exercicio.1.2.3-interpre..js
@@ -0,0 +1,19 @@
+// exercicio de interpretaçâo(1,2 e 3) - aula 8 
+/* 1- O primeiro console vai imprimir um resultado FALSE, pois o Operador E(&&) so retorna true se todos os booleanos forem true.
+
+O segundo console vai imprimir um resultado FALSE tambem.
+
+O terceiro console vai imprimir um resultado TRUE, pois o Operador NAO(!) sempre retorna o booleano oposto e o Operador OU 
+so retorna false se todos os booleanos envolvidos forem false tambem.
+
+O quarto console vai imprimir o resultado de todos os boolean.
+
+2- Pelo jeito que foi criado a estrutura do código, não haveria uma soma 
+e sim o console imprimiria um resultado ao lado do outro
+
+3- A solução para o problema seria apagar a parte de "const soma = primeiroNumero + segundoNumero"
+transformar o resultado das variaveis "primeiroNumero e segundoNumero" em numeros, usando o codigo number()
+e depois usar o console.log( primeiroNumero + segundoNumero), porque ai o console iria fazer a soma 
+ao invés de por um número ao lado do outro.
+*/
+
